71f33852ead83be65dec0b5d26ca31226e12fb3e
Jennifer Griner <TheMacMini@Jennifers-Mini.home>
2015-08-21 18:26:16 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,8 +7,9 @@
 - (void)initialize
 {
     // your code here
-}character = (Character*)[CCBReader load:@"Character"];
-[physicsNode addChild:character];
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+}
 
 -(void)update:(CCTime)delta
 {
@@ -18,5 +19,7 @@
 // put new methods here
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     // this will get called every time the player touches the screen
-}[character flap];
+    [character flap];
+}
+
 @end
